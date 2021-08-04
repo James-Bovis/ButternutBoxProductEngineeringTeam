79815452e413838e0c1195eef0a8d06e0468e183
James-Bovis <james@butternutbox.com>
2021-08-04 08:48:12 +0100

    Fetch Team Members from Slack Channel
    This commit fetches all of the Butternut Product/Engineering team members from a
    particular Slack Channel to ensure that any new team members are automatically
    added/removed.
    
    This will ensure that no hard coded User ID's are stored in the repo and they
    are instead fetched from Slack's API.

---
+++ functions/fetchConversationMembers.js
@@ -0,0 +1,19 @@
+const fetch = require('node-fetch')
+
+const { SLACK_AUTHORIZATION_TOKEN } = process.env
+
+exports.handler = async (event, context) => {
+  const channelID = event.queryStringParameters.channel
+  const API_ENDPOINT = `https://slack.com/api/conversations.members?token=${SLACK_AUTHORIZATION_TOKEN}&channel=${channelID}`
+
+  return fetch(API_ENDPOINT, { headers: { Accept: 'application/json' } })
+    .then(response => response.json())
+    .then(data => ({
+      headers: {
+        "Access-Control-Allow-Origin": "*"
+      },
+      statusCode: 200,
+      body: JSON.stringify(data.members)
+    }))
+    .catch(error => ({ statusCode: 422, body: String(error) }))
+}
